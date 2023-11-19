d1b2ad866e17fa3861f78a10f089a887900fbac0
Polski kotek <88453875+ThePolishCat@users.noreply.github.com>
2023-11-19 13:09:37 +0100

    Update updateRepo.sh

--- updateRepo.sh
+++ updateRepo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd x86_64
 repo-add catRepo.db.tar.gz *.pkg.tar.zst
-rm catRepo.db
-rm catRepo.files
+#rm catRepo.db
+#rm catRepo.files
 #rm catRepo.db.sig
 #rm catRepo.files.sig
-cp catRepo.db.tar.gz catRepo.db
-cp catRepo.files.tar.gz catRepo.files
+mv -f catRepo.db.tar.gz catRepo.db
+mv -f catRepo.files.tar.gz catRepo.files
 #cp catRepo.db.tar.gz.sig catRepo.db.sig
 #cp catRepo.files.tar.gz.sig catRepo.files.sig
 cd ..
