af21ee805c8b212214b5c23d3355a8850ac8c6f9
ThePolishCat <damianadam000@gmail.com>
2023-11-23 21:53:46 +0100

    Updated commmand

--- addPackage.sh
+++ addPackage.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "Need one package"
+    exit 1
+fi
+mkdir down
+cd down
+repoctl down $1
+cd $1
+makepkg -sr --sign --config ../../makepkg.conf
+repoctl add *.pkg.tar.zst
+cd ../../
+bash updateRepo.sh
+rm -fr down
